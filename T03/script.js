e017f8b1380cc1fd94c250d5b1bed17f205f2b42
Natalie Diva K <nataliediva7@gmail.com>
2024-04-01 18:51:22 +0700

    new stuff

---
+++ T03/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.greet, .greet-r, .greet-l, .sub').addClass('animate');
+});
+  
+function addToDisplay(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function calculate() {
+  try {
+    var result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
+}
